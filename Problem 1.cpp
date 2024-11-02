5f763ed86291223179a406e5e4349d4992cdbd32
thangtoanh26 <toanhthang26@gmail.com>
2024-11-02 23:44:48 +0700

    Update Problem 1.cpp

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -1,10 +1,22 @@
 /*
 Viết chương trình nhập vào hai số nguyên a và b. In ra 1 nếu hai số bằng nhau, in ra 0 nếu hai số khác nhau.
+
 INPUT:
 Dòng đầu tiên là số thứ nhất.
 Dòng thứ hai là số thứ hai.
-OUTPUT
+
+OUTPUT:
 Xuất ra 0 ( nếu hai số khác nhau), 1 (nếu hai số bằng nhau).
+
+VÍ DỤ:
+
+INPUT        OUTPUT
+--------------------------
+1            0
+2
+--------------------------
+3            1
+3
 */
 
 #include <stdio.h>
