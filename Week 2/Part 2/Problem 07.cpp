c1dacfa085d38642bb2d7b18e51d634324db8d61
TAThang <toanhthang26@gmail.com>
2024-11-03 12:56:52 +0700

    Update Problem 07.cpp

--- Week 2/Part 2/Problem 07.cpp
+++ Week 2/Part 2/Problem 07.cpp
@@ -1,3 +1,27 @@
+/*
+Viết chương trình nhập vào 3 số thực a, b và c. In ra các số theo thứ tự tăng dần.
+
+INPUT:
+Dòng đầu tiên là số thứ nhất.
+Dòng thứ hai là số thứ hai.
+Dòng thứ ba là số thứ ba.
+
+OUTPUT
+In ra các số theo thứ tự tăng dần.
+
+VÍ DỤ:
+INPUT                   OUTPUT
+---------------------------------------------
+2.3                     1 2.3 3.7
+1
+3.7
+---------------------------------------------
+4.9                     3.5 4.9 7.9
+7.9
+3.5
+
+*/
+
 #include <iostream>
 using namespace std;
 int main() {
