3436280afc1e1c5f7b9e29e3a702af85e15f83ff
TAThang <toanhthang26@gmail.com>
2024-11-03 12:50:01 +0700

    Update Problem 05.cpp

--- Week 2/Part 2/Problem 05.cpp
+++ Week 2/Part 2/Problem 05.cpp
@@ -1,3 +1,37 @@
+/*
+Viết chương trình phân loại nhân khẩu như sau:
+_______________________________________________________________________________
+|   Gender (Giới tính):          |      Male (Nam)      |     Female (Nữ)     |
+|   Age (Tuổi):                  |                      |                     |
+|--------------------------------|----------------------|---------------------|
+|   >=21                         |      Loại 1          |     Loại 2          |
+|--------------------------------|----------------------|---------------------|
+|   < 21                         |      Loại 3          |     Loại 4          |
+|________________________________|______________________|_____________________|
+
+INPUT
+- 1 số nguyên dương có giá trị không quá 100, chỉ tuổi của một người
+- 1 ký tự chữ cái chỉ giới tính của một người: M hoặc m (Male) = Nam, F hoặc f (Female) = Nữ
+
+OUTPUT
+- Một số nguyên chỉ mã phân loại của người đó. Ví dụ: 1, 2, 3, 4
+
+- Trường hợp không phân loại được thì xuất thông báo “I do not know why”
+
+VÍ DỤ
+Input                     Output
+----------------------------------------------
+38                        1
+M
+----------------------------------------------
+3                         I do not know why
+b
+----------------------------------------------
+35                        2
+f
+
+*/
+
 #include <iostream>
 using namespace std;
 
