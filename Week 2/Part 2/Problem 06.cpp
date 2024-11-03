0460dcba7f17963efeb163ffbe2ccad9a972cfa5
TAThang <toanhthang26@gmail.com>
2024-11-03 12:53:52 +0700

    Update Problem 06.cpp

--- Week 2/Part 2/Problem 06.cpp
+++ Week 2/Part 2/Problem 06.cpp
@@ -1,3 +1,39 @@
+/*
+Viết chương trình nhập vào điểm thi 3 môn toán, lý và hóa. Tính điểm trung bình và xếp loại học lực.
+Cách tính điểm trung bình (DTB), DTB = (điểm toán + điếm lý + điểm hóa )/3. Làm tròn 2 chữ số thập phân.
+Điều kiện xếp loại:
+    Xuất sắc (XUAT SAC) : [9-10];
+    Giỏi (GIOI): [8-9);
+    Khá (KHA): [7-8);
+    Trung bình khá (TB KHA): [6-7);
+    Trung bình (TB): [5-6);
+    Yếu (YEU): [4-5) và kém (KEM): dưới 4.
+
+INPUT
+Dòng đầu tiên là điểm Toán.
+Dòng thứ hai là điểm Lý.
+Dòng thứ ba là điểm Hóa.
+
+OUTPUT
+Xuất ra DTB và xếp loại học lực, xem ví dụ minh họa.
+
+VÍ DỤ
+INPUT                    OUTPUT
+
+8                        DTB = 8.00
+8                        Loai: GIOI
+8
+
+8                        DTB = 7.00
+6                        Loai: KHA
+7
+
+6                        DTB = 6.33
+6                        Loai: TB KHA
+7
+
+*/
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
