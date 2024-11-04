3819d4012f2bd05596f47ed0388e4e19d75c4fd3
TAThang <toanhthang26@gmail.com>
2024-11-04 21:46:17 +0700

    Thêm mô tả bài toán

--- Week 2/Part 2/Problem 10.cpp
+++ Week 2/Part 2/Problem 10.cpp
@@ -1,3 +1,33 @@
+/*
+Viết chương trình nhập vào 3 cạnh của một tam giác. Cho biết đó là loại tam giác gì?
+Biết có 5 loại: không phải tam giác, tam giác thường, cân, đều, vuông.
+Nếu là tam giác tính diện tích tam giác.
+
+INPUT:
+Dòng đầu tiên là độ dài cạnh 1.
+Dòng thứ hai là độ dài cạnh 2.
+Dòng thứ ba là độ dài cạnh 3.
+
+OUTPUT
+Định dạng như ví dụ minh họa. Diện tích làm tròn 02 chữ số sau dấu thập phân.
+
+VÍ DỤ:
+INPUT                    OUTPUT
+
+2                        Tam giac thuong, dien tich = 2.90
+3
+4
+
+3                        Tam giac vuong, dien tich = 6.00
+4
+5
+
+1                        Khong phai tam giac
+2
+3
+
+*/
+
 #include <iostream>
 #include <math.h>
 #include <iomanip>
