c7329323bb1a9ca54a9e034919adfcc945c4a09d
TAThang <toanhthang26@gmail.com>
2024-11-04 21:19:46 +0700

    Thêm mô tả bài toán Problem 8

--- Week 2/Part 2/Problem 08.cpp
+++ Week 2/Part 2/Problem 08.cpp
@@ -1,3 +1,25 @@
+/*
+Khi thi đại học, hầu hết các môn làm bài dưới dạng trắc nghiệm. Khi làm bài trắc nghiệm đôi khi kết quả có những con số rất lẻ ví dụ như đánh đúng 29/60 thì sẽ 
+được 4,8333 điểm. Trong khi đó quy chế thi đại học yêu cầu điểm các môn chỉ được giữ số lẻ đến từng 1/4. Tức là điểm phải có thể viết ở dạng a+b*(1/4) với a, b là
+hai số nguyên. Các điểm số quá lẻ như 4,83 phải được làm tròn đến số lẻ 1/4 gần nhất. Một trong những công thức phổ biến là lấy 4,83333 nhân 4. Sau đó làm tròn đến
+hàng đơn vị theo nguyên tắc thông thường rồi chia kết quả cho 4 ta có 4.75 điểm. Nhưng nếu yêu cầu là làm tròn số lẻ đế từng mức 1/n thì sao?
+
+INPUT
+Số thực a và số nguyên dương n
+
+OUTPUT
+Số thực a sau khi làm tròn đến mức lẻ 1/n. Xuất tối đa 10 số lẻ ( cout.precision(10) )
+
+VÍ DỤ
+
+Input	            Output
+4.833 4	            4.75
+1799.78959 6	    1799.833333
+5.166666667 3	    5.333333333
+5.166666666 3	    5
+
+*/
+
 #include <iostream>
 #include <cmath>
 #include <iomanip>
