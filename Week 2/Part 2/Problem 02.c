bad0564b45dc340a743d7543919b4d4cc3388dee
TAThang <toanhthang26@gmail.com>
2024-11-04 21:36:15 +0700

    Đổi tên biến + comment

--- Week 2/Part 2/Problem 02.c
+++ Week 2/Part 2/Problem 02.c
@@ -2,11 +2,11 @@
                                                                         Thu đi để lại lá vàng 
                                                                     Yagi để lại một làng tan hoang
 
-Vâng. Cơn bão Yagi vừa qua đã để lại cho đất nước chúng ta biết bao thiệt hại về con người, tài sản. Dẫu vậy, một điểm sáng tích cực mà Yagi “mang lại” cho chúng ta 
-chính là sự đoàn kết mạnh mẽ, tinh thần tương thân tương ái của nhân dân khắp mọi miền đất nước. Ngoài ra, một đại bộ phận phông bạt đã lộ diện khi được vì sao kê 
-chiếu rọi, cho thấy sự suy đồi về mặt đạo đức khi lợi dụng sự mất mát, đau thương của đồng bào để tìm kiếm sự tuyên dương, đánh bóng tên tuổi của bản thân. Người tốt 
-cần được tuyên dương và kẻ xấu cần bị lên án, bạn hãy viết một chương trình “check var” để giúp giang cư mận phát hiện “vàng thật, vàng giả” trong thời điểm khó khăn 
-hiện tại.
+Vâng. Cơn bão Yagi vừa qua đã để lại cho đất nước chúng ta biết bao thiệt hại về con người, tài sản. Dẫu vậy, một điểm sáng tích cực mà Yagi “mang lại” cho chúng
+ta chính là sự đoàn kết mạnh mẽ, tinh thần tương thân tương ái của nhân dân khắp mọi miền đất nước. Ngoài ra, một đại bộ phận phông bạt đã lộ diện khi được vì sao
+kê chiếu rọi, cho thấy sự suy đồi về mặt đạo đức khi lợi dụng sự mất mát, đau thương của đồng bào để tìm kiếm sự tuyên dương, đánh bóng tên tuổi của bản thân. Người
+tốt cần được tuyên dương và kẻ xấu cần bị lên án, bạn hãy viết một chương trình “check var” để giúp giang cư mận phát hiện “vàng thật, vàng giả” trong thời điểm khó
+khăn hiện tại.
 
 INPUT
 Hai số a, b (a, b ≤ 10^20) thể hiện số tiền quyên góp (đơn vị: nghìn đồng):
@@ -33,14 +33,15 @@ Input	              Output
 #include <bits/stdc++.h>
 
 int main() {
-    long long phongBat, saoKe;
-    scanf("%Ld%Ld", &phongBat, &saoKe);
+    long long dangTai, saoKe;
+    scanf("%Ld%Ld", &dangTai, &saoKe);
 
-    if (saoKe > phongBat || phongBat == 0) printf("Khiem ton, dang tuyen duong!");
-    else if (saoKe == phongBat) printf("Dang tuyen duong!");
-    else if (saoKe == 0) printf("Congrats! Miss Phong Bat is here!");
-    else if (phongBat % saoKe != 0) printf("Phong bat tien ung ho %.2lf lan!", phongBat * 1.0 / saoKe);
-    else printf("Phong bat tien ung ho %lld lan!", phongBat / saoKe);
+    //4 trường hợp: chiếu theo đề bài
+    if (saoKe > dangTai || dangTai == 0) printf("Khiem ton, dang tuyen duong!");
+    else if (saoKe == dangTai) printf("Dang tuyen duong!");
+    else if (saoKe == 0) printf("Congrats! Miss Phong Bat is here!")
+    else if (dangTai % saoKe != 0) printf("Phong bat tien ung ho %.2lf lan!", phongBat * 1.0 / saoKe);      //Xuất ra số thực
+    else printf("Phong bat tien ung ho %lld lan!", phongBat / saoKe);                                       //Xuất ra số nguyên
     
     return 0;
 }
