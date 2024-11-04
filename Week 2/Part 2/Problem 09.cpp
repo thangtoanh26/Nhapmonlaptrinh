d25764c70b26de78a471c4ae1adbbffcfcaed026
TAThang <toanhthang26@gmail.com>
2024-11-04 21:22:06 +0700

    Update Problem 09.cpp

--- Week 2/Part 2/Problem 09.cpp
+++ Week 2/Part 2/Problem 09.cpp
@@ -1,3 +1,31 @@
+/*
+Viết chương trình giải phương trình bậc hai: ax2 + bx + c = 0 (a ≠ 0).
+
+INPUT:
+Dòng đầu tiên là a.
+Dòng thứ hai là b
+Dòng thứ ba là c.
+Lưu ý: a, b, c thuộc kiểu số thực.
+
+OUTPUT
+Xuất ra kết quả nghiệm phương trình, xem ví dụ minh họa. (Chú ý: nếu pt có 2 nghiệm pt thì x1 và x2 in xuống dòng)
+
+VÍ DỤ:
+INPUT                    OUTPUT
+----------------------------------------------------------
+2                        PT co hai nghiem phan biet:
+0                        x1 = 1
+-2                       x2 = -1
+----------------------------------------------------------
+1                        PT co nghiem kep: x1 = x2 = -1
+2
+1
+----------------------------------------------------------
+2                        PTVN
+2
+4
+*/
+
 #include <iostream>
 #include <math.h>
 using namespace std;
