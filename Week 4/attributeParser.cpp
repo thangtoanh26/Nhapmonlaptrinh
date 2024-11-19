d1099ba534262f5fc5566c726a98dcd09b05ddbd
Tô Anh Thắng <toanhthang26@gmail.com>
2024-11-20 01:15:15 +0700

    Xây dựng khung bài phân tích thuộc tính (attribute parser)

--- Week 4/attributeParser.cpp
+++ Week 4/attributeParser.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
+
+typedef struct {
+    string tag;
+    string value;
+} tagAttr;
+
+//Khai báo hàm định nghĩa các thẻ
+vector<tagAttr> defineTags(int lines);
+//Khai báo hàm thực hiện các truy vấn
+void doQueries(vector<tagAttr>& tags, int queries);
 
 int main() {
     int lines, queries;
     cin >> lines >> queries;
 
+    //    Xóa bộ nhớ đệm
     cin.ignore();
     
+    vector<tagAttr> tags = defineTags(lines);
+    doQueries(tags, queries);
+
+    return 0;
+}
+
+vector<tagAttr> defineTags(int lines) {
+}
+
+void doQueries(vector<tagAttr>& tags, int queries) {
 }
