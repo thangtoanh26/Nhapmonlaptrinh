9db373cb8593221cc03734a0cc39cbc0a8dbc85e
Tô Anh Thắng <toanhthang26@gmail.com>
2024-11-29 20:53:24 +0700

    Update attributeParser.cpp

--- Week 4/attributeParser/attributeParser.cpp
+++ Week 4/attributeParser/attributeParser.cpp
@@ -1,8 +1,5 @@
 /*
 TODO:
-[ ] Xử lý bài toán
-    [ ] Xử lý định nghĩa tags
-    [ ] Xử lý Queries
 [ ] Đi ngủ đúng giờ
 */
 
@@ -20,7 +17,7 @@ typedef struct {
 //Khai báo hàm định nghĩa các thẻ
 vector<tagAttr> defineTags(int lines);
 //Khai báo hàm thực hiện các truy vấn
-void doQueries(vector<tagAttr>& tags, int queries);
+void doQueries(vector<tagAttr> tags, int queries);
 
 int main() {
     int lines, queries;
@@ -37,10 +34,76 @@ int main() {
 
 vector<tagAttr> defineTags(int lines) {
     vector<tagAttr> token;
+    string line, tag;
+    
+    for (int times = 0; i < lines; i++) {
+        getline(cin, line);
 
-    for (int i = 0;
+        int currentPos = 1;
+
+        if (line[currentPos] == '/') {
+            //Viết lại tag trong 2 trường hợp: tag được lồng và không được lồng
+            if (tag.rfind('.') != string::npos) 
+                //Đóng tag được lồng và quay lại tag trước đó
+                tag = tag.substr(0, tag.rfind('.'));
+            else tag.clear();
+            //Clear biến tag nếu tag không được lồng
+        }
+
+        else {
+            TagAttr element;
+            int i = 0;
+            
+            while (line[currentPos + i] != ' ' && line[currentPos + i] != '>')
+                i++;
+
+            if (tag.empty())
+                tag = line.substr(currentPos, i);
+            else 
+                tag = tag + '.' + line.substr(currentPos, i);
+            
+            currentPos += i;
+            
+            while (line[currentPos + i] != '>') {
+                currentPos++;
+                i = 0;
+                
+                while (line[currentPos + i] != ' ')
+                    i++;
+                element.tag = tag + '~' + line.substr(currentPos, i);
+                currentPos += i + 3;
+                i = 0;
+
+                while (line[currentPos + i] != ' ' && line[currentPos + i] != '>')
+                    i++;
+                element.value = line.substr(currentPos + 1, i - 2);
+
+                currentPos += i;
+                                    
+                i=0;
+                tagsAttr.push_back(element);
+            }
+        }
+    }
     return token;
 }
 
-void doQueries(vector<tagAttr>& tags, int queries) {
+void doQueries(vector<tagAttr> tags, int queries) {
+    string query;
+
+    while (queries--) {
+        getline(cin, query);
+
+        int i = 0;
+        for (auto &element : tags) {
+            i++;
+            if (query = element.tag) {
+                cout << element.value << endl;
+                break;
+            }
+
+            if (i == tags.size())
+                cout << "Not Found!" << endl;
+        }
+    }
 }
